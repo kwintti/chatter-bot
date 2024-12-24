57a6fd34ca8b5e3f68ff56ea5e4f5ae0f37cd0ef
kwintti <sergei.borovskii@gmail.com>
2024-12-25 01:40:07 +0200

    chat is now history aware. added templ

--- chatter.go
+++ chatter.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/a-h/templ"
 	"github.com/joho/godotenv"
 	openai "github.com/sashabaranov/go-openai"
 )
@@ -33,24 +34,50 @@ func loadPage(title string) *Page {
     return &Page{Title: title, Body: body}
 }
 
+type Conversation struct {
+    Messages []openai.ChatCompletionMessage
+}
+
+func newConversation() *Conversation {
+    return &Conversation{
+        Messages: []openai.ChatCompletionMessage{
+            {
+                Role: openai.ChatMessageRoleSystem,
+                Content: "Olet ride-hailing yhtiön neuvokas asiakaspalvelija. Vastaat rennosti.",
+            },
+        },
+    }
+}
+
 func main() {
     err := godotenv.Load()
     if err != nil {
         log.Println("Couldn't load .env file", err)
     }
+    component := hello("JOhn")
+    http.Handle("/", templ.Handler(component))
     // http.HandleFunc("/", handler)
     // http.HandleFunc("/view/", viewHandler)
-    // fmt.Println("Listening on :8080")
-    // log.Fatal(http.ListenAndServe(":8080", nil))
+    fmt.Println("Listening on :8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
     fmt.Println("Miten voin auttaa?:")
-    scanner := bufio.NewScanner(os.Stdin)
-    scanner.Scan()
-    err = scanner.Err()
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf(chatter(scanner.Text()))
+    for {
+        scanner := bufio.NewScanner(os.Stdin)
+        scanner.Scan()
+        err = scanner.Err()
+        if err != nil {
+            log.Fatal(err)
+        }
+        client := getClient() 
+        conv := newConversation()
+        conv.addMessage(openai.ChatMessageRoleUser, scanner.Text())
 
+        resp, err := conv.getCompletion(client)
+        if err != nil {
+            log.Fatalf("Couldn't get a response from llm: %v", err)
+        }
+        fmt.Println(resp)
+    }
 }
 
 func getClient() *openai.Client {
@@ -62,26 +89,28 @@ func getClient() *openai.Client {
     return client
 }
 
-func chatter(msg string) string {
-    client := getClient()
+func (c *Conversation) getCompletion(client *openai.Client) (string,error) {
+    client = getClient()
     resp, err := client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
-            Model: "meta-llama/Meta-Llama-3.1-8B-Instruct-fast",
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleSystem,
-					Content: msg + ". Answer this as if you are gay.",
-				},
-			},
+            Model: "meta-llama/Llama-3.3-70B-Instruct",
+			Messages: c.Messages,
 		},
 	)
 
 	if err != nil {
 		fmt.Printf("ChatCompletion error: %v\n", err)
-		return ""
+		return "", err
 	}
 
-	return resp.Choices[0].Message.Content
+	return resp.Choices[0].Message.Content, nil
 
+}
+
+func (c *Conversation) addMessage(role string, content string){
+    c.Messages = append(c.Messages, openai.ChatCompletionMessage{
+        Role: role,
+        Content: content,
+    })
 }
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/sashabaranov/go-openai v1.36.0
 )
+
+require github.com/a-h/templ v0.2.793 // indirect
